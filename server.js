f138a1e000d0a71e8b04a5408e5a338d616c86c5
demos-prog <73884833+demos-prog@users.noreply.github.com>
2024-06-10 10:28:00 +0300

    init

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from 'dotenv';
+import todo from './routes/todo.js';
+
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/todo", todo);
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}/`);
+});

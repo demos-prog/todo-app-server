9574790e2471135505bd0456c6c404f8919d76ee
demos-prog <73884833+demos-prog@users.noreply.github.com>
2024-06-19 14:10:13 +0300

    45

--- server.js
+++ server.js
@@ -6,17 +6,16 @@ import todo from './routes/todo.js';
 
 dotenv.config();
 
-const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/todo", todo);
-app.use("/", (_, res) => {
+app.get("/", (_, res) => {
   res.send('Hello from server')
 });
 
 
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}/`);
+app.listen(5050, () => {
+  console.log(`Server listening on http://localhost:5050/`);
 });
